5aed7539cf085d4a73ffa48c0dbc1ecd2958f8b2
cykreet <cykreet@gmail.com>
2025-11-12 12:31:13 +0200

    fix: replace xor method to handle non-complete trees

--- invert.cc
+++ invert.cc
@@ -2,21 +2,21 @@
 
 void invert_tree(int input[], int output[], size_t size) {
 	output[0] = input[0]; // same root node
-	int last_level_idx = 2; // index of the last node in the current level
-	int prev_idx = 0; // index of the last node from the previous level
+	int level_size = 2;
+	int prev_level_idx = 1;
 
-	while (prev_idx + 1 < size) {
+	while (prev_level_idx < size) {
 		// some recommended changes were implemented by claude that fixed a buffer overflow issue.
 		// initially, i was just iterating with i < last_level_idx, which would obviously lead to issues
 		// when last_level_idx exceeded our index range.
-		int elements_to_copy = std::min(last_level_idx, (int)size - prev_idx - 1);
+		int elements_to_copy = std::min(level_size, (int)size - prev_level_idx);
 		for (int i = 0; i < elements_to_copy; i++) {
-			int src_idx = last_level_idx - i;
-			if (src_idx > 0 && src_idx < size) output[prev_idx + 1 + i] = input[src_idx];
+			int src_idx = prev_level_idx + elements_to_copy - 1 - i;
+			if (src_idx > 0 && src_idx < size) output[prev_level_idx + i] = input[src_idx];
 		}
 		
-		prev_idx = last_level_idx;
-		last_level_idx = (last_level_idx + last_level_idx) ^ 2;
+		prev_level_idx += level_size;
+		level_size *= 2;
 	}
 }
 
@@ -29,13 +29,13 @@ bool test_inversion(int output[], int expected[], size_t size) {
 }
 
 int main() {
-	int root[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-	int expected[] = {1, 3, 2, 7, 6, 5, 4, 15, 14, 13, 12, 11, 10, 9, 8};
+	int root[] = {1,2,3,4,5,6,7,8,9,10};
+	int expected[] = {1,3,2,7,6,5,4,10,9,8};
+	// int root[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	// int expected[] = {1, 3, 2, 7, 6, 5, 4, 15, 14, 13, 12, 11, 10, 9, 8};
 	size_t root_size = std::size(root);
 	int output[root_size] = {};	
  
-	// this is reasonably fast for working with "perfect" complete binary trees, haven't tested thoroughly
-	// enough, but some performance estimates were ~10x faster than a recursive approach for trees of size 31 or less.
 	invert_tree(root, output, root_size);
 
 	if (test_inversion(output, expected, root_size))  std::cout << "Test passed!" << std::endl;
